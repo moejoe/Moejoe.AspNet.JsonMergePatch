ab3c37aefc9518fbd09bc2e224049ecf8085c07e
moejoe <m.haslhofer@gmail.com>
2017-12-23 12:04:09 +0100

    Add project files.

---
+++ Moejoe.ProofOfConcept.JsonMergePatch.Core/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Moejoe.ProofOfConcept.JsonMergePatch.Core
+{
+    public class Class1
+    {
+    }
+}
